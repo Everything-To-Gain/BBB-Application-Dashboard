d625ddd05f451b972d3c8bd795b98af9fd8c0978
Yousef Shawky <211150997+yousef-shawky-etg@users.noreply.github.com>
2025-09-12 17:10:21 +0300

    implementing form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AccreditationFormComponent } from './features/accreditation-form/pages/accreditation-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'accreditation-form',
+    pathMatch: 'full',
+  },
+  {
+    path: 'accreditation-form',
+    component: AccreditationFormComponent,
+  },
+];
