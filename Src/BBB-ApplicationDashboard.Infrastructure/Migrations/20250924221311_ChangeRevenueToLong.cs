fa686539ca17a322b8812fe43d20333008be747e
Yousef Shawky <211150997+yousef-shawky-etg@users.noreply.github.com>
2025-09-29 16:56:36 +0300

    adding migrations

---
+++ Src/BBB-ApplicationDashboard.Infrastructure/Migrations/20250924221311_ChangeRevenueToLong.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BBB_ApplicationDashboard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRevenueToLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "GrossAnnualRevenue",
+                table: "Accreditations",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "GrossAnnualRevenue",
+                table: "Accreditations",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
