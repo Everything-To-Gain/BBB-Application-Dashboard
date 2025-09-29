431e8286b5d4a2010b384b7ee1da8697a7594e91
Mahmoud Nasr <239.nasr@gmail.com>
2025-09-29 19:37:51 +0300

    Added

---
+++ Src/BBB-ApplicationDashboard.Infrastructure/Migrations/20250929142748_AuditLogsSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BBB_ApplicationDashboard.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditLogsSystem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "activity_events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    User = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Action = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Entity = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    EntityIdentifier = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    Status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    UserVersion = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Metadata = table.Column<JsonDocument>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activity_events", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activity_events");
+        }
+    }
+}
